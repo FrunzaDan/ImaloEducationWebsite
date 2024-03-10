d47397ba6fa5fc7adcd9d9924948dfccbbd0095e
FrunzaDan <frunzadan96@gmail.com>
2024-03-10 22:02:26 +0200

    revision

--- js/loadHeaderFoter.js
+++ js/loadHeaderFoter.js
@@ -1,4 +1,16 @@
-window.addEventListener('load', function () {
-    $("#navbar-placeholder").load("../html_components/navbar.html");
-    $("#footer-placeholder").load("../html_components/footer.html");
-});
+fetch("../html_components/navbar.html")
+    .then(response => response.text())
+    .then(htmlContent => {
+        document.getElementById("navbar-placeholder").innerHTML = htmlContent;
+    })
+    .catch(error => {
+        console.error("Error loading navbar content:", error);
+    });
+fetch("../html_components/footer.html")
+    .then(response => response.text())
+    .then(htmlContent => {
+        document.getElementById("footer-placeholder").innerHTML = htmlContent;
+    })
+    .catch(error => {
+        console.error("Error loading footer content:", error);
+    });
