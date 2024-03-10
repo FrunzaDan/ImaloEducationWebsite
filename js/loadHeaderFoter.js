3fb5b0a75dbb731f3150d3e1962dbe26a6201377
FrunzaDan <frunzadan96@gmail.com>
2024-03-10 21:45:35 +0200

    revision

--- js/loadHeaderFoter.js
+++ js/loadHeaderFoter.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function () {
-    $("#navbar-placeholder").load("./html_components/navbar.html");
+    $("#navbar-placeholder").load("../html_components/navbar.html");
     $("#footer-placeholder").load("../html_components/footer.html");
 });
