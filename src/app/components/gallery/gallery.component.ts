0afa47b0a0c46913d11b973cec57f3d10e20fbff
FrunzaDan <frunzadan96@gmail.com>
2024-06-05 00:20:46 +0300

    revision

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { fadeIn, fadeOut, transformIn, transformOut } from '../../animations';
 import { GalleryImage } from '../../interfaces/gallery-image';
 import { LanguageService } from '../../services/language.service';
 import { LoadGalleryService } from '../../services/load-gallery.service';
 import { SEOService } from '../../services/seo.service';
+import { SubscriptionService } from '../../services/subscription.service';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-gallery',
@@ -14,8 +16,9 @@ import { SEOService } from '../../services/seo.service';
   styleUrl: './gallery.component.css',
   animations: [transformIn, transformOut, fadeIn, fadeOut],
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnDestroy {
   public galleryImageList: GalleryImage[] = [];
+  private gallerySubscription?: Subscription;
 
   currentIndex: number = -1;
   isFullViewOpen: boolean = false;
@@ -25,7 +28,8 @@ export class GalleryComponent implements OnInit {
   constructor(
     private loadGalleryService: LoadGalleryService,
     private languageService: LanguageService,
-    private seoService: SEOService
+    private seoService: SEOService,
+    private subscriptionService: SubscriptionService
   ) {}
 
   ngOnInit(): void {
@@ -41,30 +45,38 @@ export class GalleryComponent implements OnInit {
     this.displayGalleryContent();
   }
 
-  displayGalleryContent() {
-    this.loadGalleryService.loadGallery().subscribe((fetchedImages) => {
-      this.galleryImageList = fetchedImages;
-    });
+  async displayGalleryContent() {
+    if (this.galleryImageList.length === 0) {
+      this.gallerySubscription = this.loadGalleryService
+        .loadGallery()
+        .subscribe((fetchedImages) => {
+          this.galleryImageList = fetchedImages;
+        });
+    }
   }
 
-  openFullView(index: number) {
+  openFullView(index: number): void {
     this.currentIndex = index;
     this.isFullViewOpen = true;
   }
 
-  closeFullView() {
+  closeFullView(): void {
     this.isFullViewOpen = false;
   }
 
-  navigateLeft() {
+  navigateLeft(): void {
     if (this.currentIndex > 0) {
       this.currentIndex--;
     }
   }
 
-  navigateRight() {
+  navigateRight(): void {
     if (this.currentIndex < this.galleryImageList.length - 1) {
       this.currentIndex++;
     }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionService.unsubscribeIfActive(this.gallerySubscription);
   }
 }
--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -6,7 +6,7 @@ import { Meta } from '@angular/platform-browser';
   providedIn: 'root',
 })
 export class SEOService {
-  constructor(@Inject(DOCUMENT) private doc: any, private meta: Meta) {}
+  constructor(@Inject(DOCUMENT) private doc: Document, private meta: Meta) {}
 
   updateMetaDescription(metaDescription: string): void {
     this.meta.updateTag({
@@ -15,20 +15,21 @@ export class SEOService {
     });
   }
 
-  createLinkForCanonicalURL(): void {
+  createLinkForCanonicalURL(href?: string): void {
     this.removeExistingCanonicalLink();
     let link: HTMLLinkElement = this.doc.createElement('link');
     link.setAttribute('rel', 'canonical');
     this.doc.head.appendChild(link);
-    link.setAttribute('href', this.doc.URL + '/');
+
+    link.setAttribute('href', href ? href : this.doc.URL + '/');
   }
 
   private removeExistingCanonicalLink(): void {
-    const existingLinks = this.doc.head.querySelectorAll(
+    const existingLinks: NodeListOf<Element> = this.doc.head.querySelectorAll(
       'link[rel="canonical"]'
     );
 
-    for (let i = 0; i < existingLinks.length; i++) {
+    for (let i: number = 0; i < existingLinks.length; i++) {
       this.doc.head.removeChild(existingLinks[i]);
     }
   }
