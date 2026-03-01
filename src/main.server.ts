bf1d9b64ecd8094b6446f56b0108bf9bfd1bd7ae
FrunzaDan <frunzadan96@gmail.com>
2026-03-01 19:41:13 +0200

    ng 21 update

--- src/main.server.ts
+++ src/main.server.ts
@@ -6,6 +6,13 @@ import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
 const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(AppComponent, config, context);
+  bootstrapApplication(
+    AppComponent,
+    {
+      ...config,
+      providers: [...config.providers],
+    },
+    context,
+  );
 
 export default bootstrap;
